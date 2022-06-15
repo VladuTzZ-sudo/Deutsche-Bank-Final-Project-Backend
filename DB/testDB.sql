42f228d978efc11f3edd4223d29a075e1c5dd462
Xephos76 <bogdan.isar1@gmail.com>
2022-06-15 18:33:57 +0300

    added schema to db

--- DB/testDB.sql
+++ DB/testDB.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `elearningapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `elearningapp`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: elearningapp
@@ -103,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-15 15:15:59
+-- Dump completed on 2022-06-15 18:33:37
