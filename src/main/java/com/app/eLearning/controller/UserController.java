f8f3f91e40e4312b234bc229bf97cc449d77ef49
beowolf23 <mihaiciocan44@gmail.com>
2022-06-16 11:09:06 +0300

    courses + sections dao

--- src/main/java/com/app/eLearning/controller/UserController.java
+++ src/main/java/com/app/eLearning/controller/UserController.java
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+
 @CrossOrigin
 @Controller
 public class UserController {
---
+++ src/main/java/com/app/eLearning/dao/Section.java
@@ -0,0 +1,27 @@
+package com.app.eLearning.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Section
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String title;
+
+	public Section(String title)
+	{
+		this.title = title;
+	}
+
+}
