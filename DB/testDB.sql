6df887c0c6a98845d643b25fbda58c3534c46572
Xephos76 <bogdan.isar1@gmail.com>
2022-06-17 13:20:48 +0300

    updated test db for testing get quiz from section id

--- DB/testDB.sql
+++ DB/testDB.sql
@@ -18,6 +18,140 @@ USE `elearningapp`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `answers`
+--
+
+DROP TABLE IF EXISTS `answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `answers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `answer_content` varchar(255) DEFAULT NULL,
+  `validation` bit(1) NOT NULL,
+  `id_question` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKa7obhh0b56p70kf3x0jsfdhog` (`id_question`),
+  CONSTRAINT `FKa7obhh0b56p70kf3x0jsfdhog` FOREIGN KEY (`id_question`) REFERENCES `questions` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `answers`
+--
+
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+INSERT INTO `answers` VALUES (1,'=1',_binary '\0',1),(2,'=4',_binary '',1),(3,'=3',_binary '\0',1),(4,'Sphere',_binary '',2),(5,'Cube',_binary '\0',2),(6,'Flat',_binary '\0',2),(7,'Gold Diggers',_binary '\0',3),(8,'Goal Diggers',_binary '',3),(9,'Good Diggers',_binary '\0',3);
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `id_section` int DEFAULT NULL,
+  `teacher_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKsk3c54el8i6xn2je89o4ukhqo` (`id_section`),
+  CONSTRAINT `FKsk3c54el8i6xn2je89o4ukhqo` FOREIGN KEY (`id_section`) REFERENCES `quizzes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `courses_course_sections`
+--
+
+DROP TABLE IF EXISTS `courses_course_sections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses_course_sections` (
+  `course_id` int NOT NULL,
+  `course_sections_id` int NOT NULL,
+  UNIQUE KEY `UK_cbi1hqakwo0e5swb0xbdh1uxx` (`course_sections_id`),
+  KEY `FK6rypu43jqnp36kkevhau379tb` (`course_id`),
+  CONSTRAINT `FK441g8fx1h6n87iefm0j370uvg` FOREIGN KEY (`course_sections_id`) REFERENCES `sections` (`id`),
+  CONSTRAINT `FK6rypu43jqnp36kkevhau379tb` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+  CONSTRAINT `FKn78kvlqdahekj9cy2ck5iovgy` FOREIGN KEY (`course_sections_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses_course_sections`
+--
+
+LOCK TABLES `courses_course_sections` WRITE;
+/*!40000 ALTER TABLE `courses_course_sections` DISABLE KEYS */;
+/*!40000 ALTER TABLE `courses_course_sections` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `questions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content_question` varchar(255) DEFAULT NULL,
+  `id_quiz` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKevbiobobyiwgpro3ogd3tumk5` (`id_quiz`),
+  CONSTRAINT `FKevbiobobyiwgpro3ogd3tumk5` FOREIGN KEY (`id_quiz`) REFERENCES `quizzes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'What is 2+2?',1),(2,'What shape is Earth?',1),(3,'Team name?',2);
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quizzes`
+--
+
+DROP TABLE IF EXISTS `quizzes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quizzes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `quiz_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quizzes`
+--
+
+LOCK TABLES `quizzes` WRITE;
+/*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
+INSERT INTO `quizzes` VALUES (1,'Essential Quiz 1'),(2,'Advanced Quiz');
+/*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `roles`
 --
 
@@ -42,6 +176,36 @@ INSERT INTO `roles` VALUES (1,'teacher'),(2,'student');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `sections`
+--
+
+DROP TABLE IF EXISTS `sections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sections` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `id_section` int DEFAULT NULL,
+  `quiz_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKa8a1dxhqntcil81jfo8l22l5c` (`id_section`),
+  KEY `FKtlxxk00psgcjxhmdco5yfyf7c` (`quiz_id`),
+  CONSTRAINT `FKa8a1dxhqntcil81jfo8l22l5c` FOREIGN KEY (`id_section`) REFERENCES `quizzes` (`id`),
+  CONSTRAINT `FKtlxxk00psgcjxhmdco5yfyf7c` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sections`
+--
+
+LOCK TABLES `sections` WRITE;
+/*!40000 ALTER TABLE `sections` DISABLE KEYS */;
+INSERT INTO `sections` VALUES (1,'first section',1,1),(2,'second section',1,2),(3,'third section',1,NULL);
+/*!40000 ALTER TABLE `sections` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user_role`
 --
 
@@ -52,7 +216,7 @@ CREATE TABLE `user_role` (
   `id` int NOT NULL AUTO_INCREMENT,
   `role_id` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,7 +225,7 @@ CREATE TABLE `user_role` (
 
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` VALUES (1,1),(2,2),(3,1),(4,1);
+INSERT INTO `user_role` VALUES (1,1),(2,1);
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,7 +247,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `FK7x3uo1krtxr8r60py9rd2ys5p` (`user_role_id`),
   CONSTRAINT `FK7x3uo1krtxr8r60py9rd2ys5p` FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,8 +256,34 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,NULL,'jack@gmail.com','Jack','12345','Black',1),(2,_binary '','mike@gmail.com','Mike','54321','Tison',2),(3,_binary '','jim@gmail.com','Jim','11111','Carrey',3),(4,NULL,'mark@gmail.com0000000000000000','Mark','11111','Zucky',4);
+INSERT INTO `users` VALUES (1,_binary '','mike@gmail.com','Mike','$2a$10$xi905qiaVEdQZmi6VRlI8OlW3qVcZHycQoyGAET3TqJAoHlJ8EC9S','Jackson',1),(2,NULL,'mi@gmail.com','Mi','$2a$10$6V1GmV3MPnP4/.eJzLcbsea6zT5KwnJnck04kCUlVL0YOCwZgbFJS','Jackson',2);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_user_courses`
+--
+
+DROP TABLE IF EXISTS `users_user_courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users_user_courses` (
+  `user_id` int NOT NULL,
+  `user_courses_id` int NOT NULL,
+  KEY `FKo279fqge4rdl9tu8srs7n25hj` (`user_courses_id`),
+  KEY `FKkknpt97pd2habxr07nbr9smay` (`user_id`),
+  CONSTRAINT `FKkknpt97pd2habxr07nbr9smay` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKo279fqge4rdl9tu8srs7n25hj` FOREIGN KEY (`user_courses_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_user_courses`
+--
+
+LOCK TABLES `users_user_courses` WRITE;
+/*!40000 ALTER TABLE `users_user_courses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_user_courses` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -105,4 +295,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-15 18:51:32
+-- Dump completed on 2022-06-17 13:20:09
